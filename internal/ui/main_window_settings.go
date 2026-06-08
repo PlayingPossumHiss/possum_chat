1bd0f07ada8f3027023df0e4a1f20770333dda5c
playing possum <gadalkainga@yandex.ru>
2026-06-08 22:52:55 +0300

    [17] Поддержка кика

--- internal/ui/main_window_settings.go
+++ internal/ui/main_window_settings.go
@@ -14,7 +14,7 @@ import (
 	"github.com/PlayingPossumHiss/possum_chat/internal/service/logger"
 )
 
-const version = "v1.2.2"
+const version = "7632d91"
 
 func (ui *UI) getSettingsTabContent() *fyne.Container {
 	const itemsInLine = 2
