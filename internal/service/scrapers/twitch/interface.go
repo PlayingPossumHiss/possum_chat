5b94611aa82d5b99d0ac08e1f6aee5e2dfc77579
playing possum <gadalkainga@yandex.ru>
2026-09-14 21:12:14 +0300

    [73] Добавил онлайн твича

--- internal/service/scrapers/twitch/interface.go
+++ internal/service/scrapers/twitch/interface.go
@@ -1,12 +1,20 @@
 package twitch
 
-import "github.com/PlayingPossumHiss/possum_chat/internal/entity"
+import (
+	"context"
+
+	"github.com/PlayingPossumHiss/possum_chat/internal/entity"
+)
 
 type TwitchIrcClient interface {
 	Listen(
 		channelName string,
 	) chan entity.Message
 	Close() error
+}
+
+type TwitchClient interface {
+	GetOnline(ctx context.Context, channelName string) (int64, error)
 }
 
 type ConfigStorage interface {
